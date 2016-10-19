8d8032c5d82cd78bf1bdc35d192a728b552c5d76
Jeremy Chang <shunyu.chang@gmail.com>
2016-10-19 12:30:28 +0800

    added a onclick function on widget view

--- app/src/main/java/com/example/jeremy/widgetsample/NewAppWidget.java
+++ app/src/main/java/com/example/jeremy/widgetsample/NewAppWidget.java
@@ -1,8 +1,10 @@
 package com.example.jeremy.widgetsample;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 import android.widget.RemoteViews;
 
@@ -20,6 +22,12 @@ public class NewAppWidget extends AppWidgetProvider {
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
         views.setTextViewText(R.id.appwidget_text, widgetText);
 
+        // Create an Intent to launch ExampleActivity
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+        //RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
+        views.setOnClickPendingIntent(R.id.appwidget_text, pendingIntent);
         // Instruct the widget manager to update the widget
         Log.d(TAG, "Update NewAppWidget: appWidgetId:" + appWidgetId);
         appWidgetManager.updateAppWidget(appWidgetId, views);
@@ -27,6 +35,9 @@ public class NewAppWidget extends AppWidgetProvider {
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        final int N = appWidgetIds.length;
+
+        Log.d(TAG, "onUpdate appWidgetIds.length=" + N);
         // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);

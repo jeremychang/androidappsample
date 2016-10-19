340cede4f10be497acfa2549d9a5b8f3e190b7dd
Jeremy Chang <shunyu.chang@gmail.com>
2016-10-19 15:35:37 +0800

    Activity1 button onclick to start activity Activity2

--- app/src/main/java/com/example/jeremy/widgetsample/MainActivity.java
+++ app/src/main/java/com/example/jeremy/widgetsample/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.jeremy.widgetsample;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new Button.OnClickListener(){
+                                             @Override
+                                             public void onClick(View view) {
+                                                 sendMessage(view);
+                                             }
+                                         }
+        );
     }
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, Main2Activity.class);
+
+        //EditText editText = (EditText) findViewById(R.id.edit_message);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
 }

3c395dd188a21a8d22580f5fe0baeec7f8420ae4
Jeremy Chang <shunyu.chang@gmail.com>
2016-10-20 14:38:06 +0800

    MapsActiviity: added MyLocation layer

--- app/src/main/java/com/example/jeremy/appsample/MapsActivity.java
+++ app/src/main/java/com/example/jeremy/appsample/MapsActivity.java
@@ -1,7 +1,12 @@
 package com.example.jeremy.appsample;
 
-import android.support.v4.app.FragmentActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,9 +15,12 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
+    public final static String TAG = "MapsActivity";
     private GoogleMap mMap;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    final LatLng TAIPEI101 = new LatLng(25.033611, 121.565000);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,9 +46,37 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Add a marker in Taipei 101 and move the camera
+        mMap.addMarker(new MarkerOptions().position(TAIPEI101).title("Taipei 101"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(TAIPEI101));
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "Permission was GRANTED and enable my location");
+            mMap.setMyLocationEnabled(true);
+        } else {
+            // Show rationale and request permission.
+            PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
+                    Manifest.permission.ACCESS_FINE_LOCATION, false);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (permissions.length == 1 &&
+                    permissions[0] == Manifest.permission.ACCESS_FINE_LOCATION &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.d(TAG, "Permission was GRANTED");
+
+                mMap.setMyLocationEnabled(true);
+
+            } else {
+                // Permission was denied. Display an error message.
+                Log.d(TAG, "Permission was denied");
+            }
+        }
     }
 }

ddc981095a5337a96e2a4dcf54d2bca8b01ff31f
Jeremy Chang <shunyu.chang@gmail.com>
2016-10-20 20:48:20 +0800

    Activity2: added a keyEvent callback

--- app/src/main/java/com/example/jeremy/appsample/Main2Activity.java
+++ app/src/main/java/com/example/jeremy/appsample/Main2Activity.java
@@ -3,7 +3,9 @@ package com.example.jeremy.appsample;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -19,6 +21,16 @@ public class Main2Activity extends AppCompatActivity {
         String name = b.getString(MainActivity.INPUT_NAME);
         textView.setText(name);
         Log.i(TAG, "onCreate");
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        switch(keyCode) {
+            case KeyEvent.KEYCODE_BACK:
+                // Show a Toast to say Back:
+                Toast.makeText(this, "Back!", Toast.LENGTH_SHORT).show();
+        }
+        return super.onKeyUp(keyCode, event);
     }
 
     @Override

efbcdcbd1aabe7e4f50ba68d4584f149e3bec95c
Jeremy Chang <shunyu.chang@gmail.com>
2016-11-29 16:20:26 +0800

    WebView Activity: added a AutoCompleteTextView

--- app/src/main/java/com/example/jeremy/appsample/WebActivity.java
+++ app/src/main/java/com/example/jeremy/appsample/WebActivity.java
@@ -10,6 +10,8 @@ import android.view.inputmethod.EditorInfo;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -24,12 +26,24 @@ public class WebActivity extends AppCompatActivity {
     public final static String TAG = "WebActivity";
     private String mUrl = "http://google.com";
 
-    @BindView(R.id.button_submit)
-    Button mButtonSubmit;
+    @BindView(R.id.autoCompleteTextView)
+    AutoCompleteTextView mACTextView;
+    @BindView(R.id.button_submit1)
+    Button mButtonSubmit1;
     @BindView(R.id.edit_text)
     EditText mEditText;
+    @BindView(R.id.button_submit2)
+    Button mButtonSubmit2;
     @BindView(R.id.webview)
     WebView mWebView;
+
+    static final String BASE = "http://";
+    static final String[] URLS = {
+            BASE + "localhost:8080",
+            BASE + "google.com",
+            BASE + "github.com",
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,21 +55,32 @@ public class WebActivity extends AppCompatActivity {
         mWebView.setWebViewClient(new MyWebViewClient());
         mWebView.getSettings().getJavaScriptEnabled();
         mWebView.loadUrl(mUrl);
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>
+                (this, android.R.layout.simple_list_item_1, URLS);
+        mACTextView.setAdapter(adapter);
     }
 
     @OnEditorAction(R.id.edit_text)
     public boolean onEditorAction (TextView v, int actionId, KeyEvent event) {
         // To specify the keyboard action button, use the android:imeOptions attribute
         if (actionId == EditorInfo.IME_ACTION_DONE) {
-            mButtonSubmit.performClick();
+            mButtonSubmit2.performClick();
             return true;
         }
         return false;
     }
 
-    @OnClick(R.id.button_submit)
-    public void button_submit(View view) {
+    @OnClick(R.id.button_submit2)
+    public void button_submit2(View view) {
         mUrl = mEditText.getText().toString();
+        mWebView.loadUrl(mUrl);
+    }
+
+    @OnClick(R.id.button_submit1)
+    public void button_submit1(View view) {
+        mUrl = mACTextView.getText().toString();
         mWebView.loadUrl(mUrl);
     }
 
